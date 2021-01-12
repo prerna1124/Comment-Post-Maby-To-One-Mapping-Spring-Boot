18279cc93c1dd00a9ca02902eec96865be453ee8
Prerna Sharma <prernakaruna24@gmail.com>
2021-01-12 13:19:25 +0530

    Added API the fetch All posts and fetch post by Id

--- src/main/java/com/manytoone/controller/Controller.java
+++ src/main/java/com/manytoone/controller/Controller.java
@@ -3,10 +3,11 @@ package com.manytoone.controller;
 import com.manytoone.model.Post;
 import com.manytoone.service.ServiceClass;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +20,15 @@ public class Controller {
     public Post addPost(@RequestBody Post post)
     {
         return service.addPost(post);
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable long id){
+        return service.getPostById(id);
+    }
+
+    @GetMapping("/findAll")
+    public List<Post> getAllPosts(){
+        return service.getAllPosts();
     }
 }
--- src/main/java/com/manytoone/service/ServiceClass.java
+++ src/main/java/com/manytoone/service/ServiceClass.java
@@ -3,7 +3,11 @@ package com.manytoone.service;
 import com.manytoone.model.Post;
 import com.manytoone.repository.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServiceClass {
@@ -13,5 +17,15 @@ public class ServiceClass {
 
     public Post addPost(Post post) {
         return repo.save(post);
+    }
+
+    public ResponseEntity<Post> getPostById(long id) throws ResourceNotFoundException{
+        Post post = repo.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Post not found with id "+id));
+        return ResponseEntity.ok(post);
+    }
+
+    public List<Post> getAllPosts() {
+        return repo.findAll();
     }
 }
